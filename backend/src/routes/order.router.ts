359435b330b4722dd6bc3ffe8fce5638321a8ba9
Andrey Krivosheev <andreykrt12@gmail.com>
2025-09-20 01:29:15 +0300

    Add orderSchema

--- backend/src/routes/order.router.ts
+++ backend/src/routes/order.router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import addOrder from '../controllers/order.controller';
+import validateBody from '../middlewares/validation';
+import OrderSchema from '../validation/order.validation';
 
 const orderRouter = Router();
 
-orderRouter.post('/', addOrder);
+orderRouter.post('/', validateBody(OrderSchema), addOrder);
 
 export default orderRouter;
